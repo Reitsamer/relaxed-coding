1bc0aba7158cd9881bd4c1eb1b364a4f1d26f1d6
Wolf Reitsamer <reit@htl-donaustadt.at>
2020-03-19 17:01:53 +0100

    Multi-Threading with Delegates (Part 1)

---
+++ c#/ThreadsWithDelegates/ThreadsWithDelegates/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadsWithDelegates
+{
+    class Program
+    {
+        public delegate int BinaryOp(int x, int y);
+
+        private static int Add(int a, int b)
+        {
+            Console.WriteLine($"Add() executed on thread #{Thread.CurrentThread.ManagedThreadId}");
+
+            Thread.Sleep(4000);
+            return a + b;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Main() executed on thread #{Thread.CurrentThread.ManagedThreadId}");
+
+            BinaryOp op = Add;
+
+            //int result = op.Invoke(10, 10);
+
+            // Thread thread = new Thread(Add);
+            IAsyncResult asyncResult = op.BeginInvoke(10, 10, null, null);
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Thread.Sleep(500);
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+
+            // thread.Join();
+            // int result = op.EndInvoke(asyncResult);
+            // Console.WriteLine($"The result of 10 + 10 is: {result} (thread #{Thread.CurrentThread.ManagedThreadId})");
+
+            Console.ReadLine();
+        }
+    }
+}
